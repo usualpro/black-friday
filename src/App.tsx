da24dc820f74f908b08ba9894e33718867f531c5
Gary FELICITE-PRUDENT <gary.felicite-prudent@teladoc.com>
2023-07-07 23:18:14 +0200

    update for desktop

--- src/App.tsx
+++ src/App.tsx
@@ -12,17 +12,17 @@ import {
 function App() {
   return (
     <main className="">
-      <section style={{ height: "230px" }}>
+      <section className="position-relative">
+        <div className="position-absolute absolute-centering">
+          <img src="assets/svg/i.svg" alt="" />
+        </div>
         <div className="grid-col-6 h-100">
           <div className="d-flex p-1 justify-content-center  bg-yellow">
             <div className="align-self-center">
               <div>
-                <div className="vstack text-center p-3 ">
+                <div className="vstack text-center p-1 gap-1">
                   <div>
-                    <h4 className="m-0">plus que</h4>
-                  </div>
-                  <div>
-                    <span>00 00 00 00</span>
+                    <img src="assets/svg/countdown.svg" alt="" />
                   </div>
                   <div>
                     <h3 className="heading font-size-42px line-height-38px">
@@ -47,52 +47,58 @@ function App() {
         </div>
       </section>
       <section>
-        <div className="text-center bg-white p-2 pt-0">
-          <div className="hstack justify-content-center">
-            <img src="assets/svg/product-addon.svg" alt="" />
-            <img src="assets/svg/bike.svg" className="img-fluid" alt="" />
+        <div className="align-self-center justify-content-center d-flex">
+          <div className="d-none d-md-flex font-size-0">
+            <img src="assets/jpg/good-plan.jpg" alt="" className="img-fluid" />
           </div>
-
-          <div className="vstack gap-2">
-            <div>
-              <span className="patch">-300$</span>
+          <div className="bg-white d-flex flex-direction-column p-2 text-center">
+            <div className="hstack justify-content-center">
+              <img src="assets/svg/product-addon.svg" alt="" />
+              <img src="assets/svg/bike.svg" className="img-fluid" alt="" />
             </div>
-            <div className="vstack gap-1">
-              <div className="text-center">
-                <div className="text-grey-light">
-                  <div className="font-weight-400">Adulte</div>
-                  <div className="text-uppercase font-weight-700">nakamura</div>
-                  <div className="font-weight-400 ">
-                    <div>VTT électrique E-CLIFF LTD</div>
+            <div className="vstack gap-2">
+              <div>
+                <span className="patch">-300$</span>
+              </div>
+              <div className="vstack gap-1">
+                <div className="text-center">
+                  <div className="text-grey-light">
+                    <div className="font-weight-400">Adulte</div>
+                    <div className="text-uppercase font-weight-700">
+                      nakamura
+                    </div>
+                    <div className="font-weight-400 ">
+                      <div>VTT électrique E-CLIFF LTD</div>
+                    </div>
                   </div>
                 </div>
+                <div className="text-center">
+                  <span className="font-weight-400">
+                    <del className="font-size-12px">999,99$</del>
+                  </span>{" "}
+                  <span className="text-red font-size-20px font-weight-700">
+                    699,99$
+                  </span>
+                </div>
+                <div className="position-relative justify-content-center gap-1 hstack">
+                  <span className="bull">
+                    <span>4x</span>
+                  </span>
+                  <span>Ou payer en 4x sans frais</span>
+                </div>
               </div>
-              <div className="text-center">
-                <span className="font-weight-400">
-                  <del className="font-size-12px">999,99$</del>
-                </span>{" "}
-                <span className="text-red font-size-20px font-weight-700">
-                  699,99$
-                </span>
+              <div>
+                <button className="btn primary">Acheter</button>
               </div>
-              <div className="position-relative justify-content-center gap-1 hstack">
-                <span className="bull">
-                  <span>4x</span>
-                </span>
-                <span>Ou payer en 4x sans frais</span>
-              </div>
+              <div>&bull; &bull; &bull; &bull;</div>
             </div>
-            <div>
-              <button className="btn primary">Acheter</button>
-            </div>
-            <div>&bull; &bull; &bull; &bull;</div>
           </div>
         </div>
       </section>
       <section>
         <Menu />
       </section>
-      <section>
+      <section className="d-none-md bg-grey-lighter">
         <div
           className="p-2 bg-size-cover bg-no-repeat bg-position-center"
           style={{
@@ -112,8 +118,11 @@ function App() {
           </div>
         </div>
       </section>
-      <section style={{ backgroundColor: "var(--semantics-white-bg)" }}>
-        <div className="vstack">
+      <section
+        style={{ backgroundColor: "var(--semantics-white-bg)" }}
+        className="d-none-md"
+      >
+        <div className="vstack ">
           <div className="p-2">
             <div>
               <span className="patch">-60$</span>
